d573fdab19c024b4f969cd7e5b810fa772f64832
augenster <hoanghoa38sos@gmail.com>
2022-03-07 07:18:12 +0700

    Updates

---
+++ precache-manifest.12f6629a699daf682b7d17c9c5e8fdf9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "f311eacfaf0e65cb1da8",
+    "url": "/augenstern.github.io/static/js/0.f311eacf.chunk.js"
+  },
+  {
+    "revision": "2a548399ea9b8451e563",
+    "url": "/augenstern.github.io/static/css/main.bb55430b.chunk.css"
+  },
+  {
+    "revision": "2a548399ea9b8451e563",
+    "url": "/augenstern.github.io/static/js/main.2a548399.chunk.js"
+  },
+  {
+    "revision": "82aa8f3938daf0bb17b5",
+    "url": "/augenstern.github.io/static/css/2.4bd47c9b.chunk.css"
+  },
+  {
+    "revision": "82aa8f3938daf0bb17b5",
+    "url": "/augenstern.github.io/static/js/2.82aa8f39.chunk.js"
+  },
+  {
+    "revision": "ea425760f32bb0ec1b94",
+    "url": "/augenstern.github.io/static/css/3.a3e69bd8.chunk.css"
+  },
+  {
+    "revision": "ea425760f32bb0ec1b94",
+    "url": "/augenstern.github.io/static/js/3.ea425760.chunk.js"
+  },
+  {
+    "revision": "90042573dc96740d77d7",
+    "url": "/augenstern.github.io/static/js/4.90042573.chunk.js"
+  },
+  {
+    "revision": "6374dc8d2a9f674f5c70",
+    "url": "/augenstern.github.io/static/css/5.35e0a149.chunk.css"
+  },
+  {
+    "revision": "6374dc8d2a9f674f5c70",
+    "url": "/augenstern.github.io/static/js/5.6374dc8d.chunk.js"
+  },
+  {
+    "revision": "d3621c3855e63fcdf591",
+    "url": "/augenstern.github.io/static/css/6.f4c3fd15.chunk.css"
+  },
+  {
+    "revision": "d3621c3855e63fcdf591",
+    "url": "/augenstern.github.io/static/js/6.d3621c38.chunk.js"
+  },
+  {
+    "revision": "87fed5b1931c774d30e6",
+    "url": "/augenstern.github.io/static/js/7.87fed5b1.chunk.js"
+  },
+  {
+    "revision": "f5095081b17306255487",
+    "url": "/augenstern.github.io/static/css/8.864ae015.chunk.css"
+  },
+  {
+    "revision": "f5095081b17306255487",
+    "url": "/augenstern.github.io/static/js/8.f5095081.chunk.js"
+  },
+  {
+    "revision": "1f9cb6c35ce40ff4e9c4",
+    "url": "/augenstern.github.io/static/js/9.1f9cb6c3.chunk.js"
+  },
+  {
+    "revision": "5b6d78a5bd3aba4cb4fd",
+    "url": "/augenstern.github.io/static/js/10.5b6d78a5.chunk.js"
+  },
+  {
+    "revision": "efc6d83a5fc8b29a3b2c",
+    "url": "/augenstern.github.io/static/js/11.efc6d83a.chunk.js"
+  },
+  {
+    "revision": "50265907a8cf7b6dc98e",
+    "url": "/augenstern.github.io/static/js/runtime~main.50265907.js"
+  },
+  {
+    "revision": "53d6ecb2c04ed9f53b94045a4d62bc21",
+    "url": "/augenstern.github.io/static/media/cart.53d6ecb2.png"
+  },
+  {
+    "revision": "16f12759e60071709496dcb9d3f3cb02",
+    "url": "/augenstern.github.io/static/media/img-1.16f12759.png"
+  },
+  {
+    "revision": "71afc93135f72777ea667a2b9dfb650f",
+    "url": "/augenstern.github.io/static/media/img-2.71afc931.png"
+  },
+  {
+    "revision": "5870f6701023a4c687b8bd1c7d56dd91",
+    "url": "/augenstern.github.io/static/media/img-3.5870f670.png"
+  },
+  {
+    "revision": "6822dd515420ba640ee659f722020144",
+    "url": "/augenstern.github.io/static/media/login.6822dd51.png"
+  },
+  {
+    "revision": "91e5a0bbeb3c9112bfd5ecf1c6c0d172",
+    "url": "/augenstern.github.io/static/media/signin.91e5a0bb.png"
+  },
+  {
+    "revision": "4fe401d528dee33a6bf2b0854d29aac0",
+    "url": "/augenstern.github.io/static/media/user.4fe401d5.png"
+  },
+  {
+    "revision": "fe1eb62fc358713303a8fa86c87ce288",
+    "url": "/augenstern.github.io/static/media/love.fe1eb62f.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/augenstern.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/augenstern.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/augenstern.github.io/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/augenstern.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "db62f229d163c30871e12a12a5ff1553",
+    "url": "/augenstern.github.io/index.html"
+  }
+];
